fbb655bd5ba12718a5b791c611ed8fa1f3c9d83b
platon777 <wichleyvalentin@gmail.com>
2022-03-04 14:57:40 +0100

    inspect db and connecting app with mysql

--- ayitiweb_market/settings.py
+++ ayitiweb_market/settings.py
@@ -86,8 +86,13 @@ WSGI_APPLICATION = 'ayitiweb_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ayitiweb_market',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3307',
+        'USER' : 'root',
+        'PASSWORD' : 'root'
+        
     }
 }
 
