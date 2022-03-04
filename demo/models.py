40deb72066dfcec16897187c42bee16d0adb7125
platon777 <wichleyvalentin@gmail.com>
2022-03-04 16:10:53 +0100

    customizing models and make migrations

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,198 @@
 from django.db import models
 
-# Create your models here.
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add= True)
+
+    class Meta:
+        managed = False
+        db_table = 'cart'
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey('Product', on_delete= models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete= models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'cart_item'
+        unique_together = (('product', 'cart'),)
+
+
+class Collection(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=45)
+
+    class Meta:
+        managed = False
+        db_table = 'collection'
+
+
+class Customer(models.Model):
+   # MEMBERSHIP_CHOICES = [ ('B','Bronze'), ('S','Silver'), ('G', 'Gold')]    
+    
+    first_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True, max_length=255)
+    phone = models.CharField(max_length=45)
+    dob = models.DateField(blank=True, null=True)
+    #membership = models.CharField(max_length=1, choices= MEMBERSHIP_CHOICES, default= 'B', blank=True, null=True )
+
+    class Meta:
+        managed = False
+        db_table = 'customer'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_CHOICES = [ ('P','Pending'), ('C','Completed'), ('F', 'Failed')]
+    
+    placed_at = models.DateTimeField(auto_now_add = True)
+    payment_status = models.CharField(max_length = 1, choices = PAYMENT_STATUS_CHOICES, default= 'P')
+    customer = models.ForeignKey(Customer, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'order'
+        unique_together = (('id', 'customer'),)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey('Product', on_delete = models.PROTECT)
+    order = models.ForeignKey(Order, on_delete = models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits= 6, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'order_item'
+        unique_together = (('product', 'order'),)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits= 6 ,decimal_places= 2)
+    inventory = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now= True)
+    collection = models.ForeignKey(Collection, on_delete = models.PROTECT)
+
+    class Meta:
+        managed = False
+        db_table = 'product'
+        unique_together = (('id', 'collection'),)
